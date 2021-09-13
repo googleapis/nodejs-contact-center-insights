7bb408119e9a31a15d634fa09c90bf79510a82f9
Bamboo Le <trucle@google.com>
2021-09-13 18:38:18 -0400

    update log message

--- samples/setProjectTtl.js
+++ samples/setProjectTtl.js
@@ -42,7 +42,7 @@ function main(projectId) {
         paths: ['conversation_ttl'],
       },
     });
-    console.info('Set TTL for all incoming conversations to 60 seconds.');
+    console.info('Set TTL for all incoming conversations to 60 seconds');
   }
   setProjectTtl();
   // [END contactcenterinsights_set_project_ttl]
